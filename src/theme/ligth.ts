d47697300ea4a980f36d62057358ef19fda9a41f
ArielBetti <ariel.betti@gmail.com>
2022-09-02 20:09:45 -0300

    feat(theme): create light theme base

--- src/theme/ligth.ts
+++ src/theme/ligth.ts
@@ -2,6 +2,26 @@ import { ITheme } from "./types";
 
 export const ligth = (): Partial<ITheme> => ({
   colors: {
-    background: "#FAFAFA",
+    background: "#f7f7f7",
+    neutral: {
+      pure: "#ffffff",
+      1: "#efeff1",
+      2: "#e5e5e5",
+      3: "#eaeaea",
+    },
+    primary: "#9147ff",
+    secondary: "#FEC260",
+  },
+  font: {
+    colors: {
+      contrast: "#9147ff",
+      1: "#0E0E10",
+      2: "#1F1F23",
+      3: "#000000B3",
+      dark: "#000",
+      white: "#FFF",
+      inverse: "#FFF",
+      pure: "#000",
+    },
   },
 });
